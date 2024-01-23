2363e7789a4023fa99faa80e2c8c821c0efea45a
Goodsol Lee <33349919+lgs96@users.noreply.github.com>
2024-01-23 02:49:25 -0500

    Updated for SDK 34.0

--- app/src/main/java/snu/nxc/testtcp/MainActivity.java
+++ app/src/main/java/snu/nxc/testtcp/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.goodsol.R;
+
 import snu.nxc.testtcp.ClientAgent.ClientAgentListener;
 
 public class MainActivity extends AppCompatActivity {
